bc9bec5f73b595351d3ef07a3d78801ca2fe63ca
Grigiriy <grigiriy.malyshev@gmail.com>
2020-04-01 20:17:54 +0300

    favicon etc

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
+import Helmet from 'react-helmet';
 
 import '../../assets/scss/styles.scss';
 
@@ -10,6 +11,27 @@ class Layout extends Component {
     let { children } = this.props;
     return (
       <>
+        <Helmet
+          title="Pick-a-dog - Возьми собаку в аренду"
+          meta={[
+            {
+              name: 'description',
+              content:
+                'В условиях карантина важен каждый миг проведенный вне дома. Мы запустили специальный маркетплейс для сдачи собак в аренду специально к 1 апреля.'
+            },
+            {
+              name: 'keywords',
+              content: 'карантин, самоизоляция, собаки, собака в аренду'
+            }
+          ]}
+          link={[
+            {
+              rel: 'shortcut icon',
+              type: 'image/png',
+              href: `${require('../../images/favicon.png')}`
+            }
+          ]}
+        />
         <Header />
         <div className="z-0 container-fluid position-relative">{children}</div>
         <Footer />
