e7fc2aa7f69678b7a26c02e5637bced1256a7903
Grigiriy <grigiriy.malyshev@gmail.com>
2020-04-20 19:29:08 +0300

    pageGen

---
+++ gatsby-node.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions;
+
+  const result = await graphql(`
+    query {
+      allDogsJson(sort: { fields: name, order: ASC }) {
+        nodes {
+          id
+          name
+          main
+          secondary
+          image
+          parent {
+            ... on File {
+              name
+              relativeDirectory
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  if (result.errors) {
+    reporter.panicOnBuild('🚨  ERROR: Loading "createPages" query');
+  }
+
+  const dogs = result.data.allDogsJson.nodes;
+
+  dogs.forEach((item) => {
+    createPage({
+      path: item.parent.name,
+      component: path.resolve(`./src/components/DogPage/index.js`),
+      context: { dog: item },
+    });
+  });
+};
