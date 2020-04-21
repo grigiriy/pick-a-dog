5e05db5f16eb555953a5fc94c3a7e74ea8d81977
Grigiriy <grigiriy.malyshev@gmail.com>
2020-04-22 00:27:27 +0300

    map coords etc

--- src/components/YMap/YMap.jsx
+++ src/components/YMap/YMap.jsx
@@ -1,6 +1,25 @@
 import React, { Component } from 'react';
-import { YMaps, Map } from 'react-yandex-maps';
+import { YMaps, Map, Placemark } from 'react-yandex-maps';
+import { graphql, StaticQuery } from 'gatsby';
 
+import dogface from '../../images/favicon.png';
+
+const allDogsQuery = graphql`
+  query {
+    allDogsJson(sort: { fields: name, order: ASC }) {
+      nodes {
+        id
+        name
+        geo
+        parent {
+          ... on File {
+            name
+          }
+        }
+      }
+    }
+  }
+`;
 class YMap extends Component {
   render() {
     let { children } = this.props;
@@ -9,19 +28,43 @@ class YMap extends Component {
         <div
           className="position-fixed"
           style={{
-            top: 0
+            top: 0,
           }}
         >
           <Map
             style={{
               minWidth: `50vw`,
-              minHeight: `100vh`
+              minHeight: `100vh`,
             }}
             state={{
               center: [55.67411, 37.608526],
-              zoom: 12
+              zoom: 12,
+            }}
+          >
+          <StaticQuery
+            query={allDogsQuery}
+            render={(data) => {
+              return data.allDogsJson.nodes.map((item) => (
+                <Placemark key={item.id}
+                geometry={item.geo}
+
+                properties={{
+                  hintContent: 'Собственный значок метки',
+                  balloonContent: 'ХУЙ'
+                }}
+
+                options={{
+                  iconLayout: 'default#image',
+                  iconImageHref: dogface,
+                  iconImageSize: [50, 50],
+                  iconImageOffset: [-3, -42],
+                }}
+
+                />
+              ));
             }}
           />
+          </Map>
           {children}
         </div>
       </YMaps>
@@ -29,3 +72,4 @@ class YMap extends Component {
   }
 }
 export default YMap;
+
